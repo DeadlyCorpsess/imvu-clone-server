2a17099816e262a1083fd45222b82c0521eb417d
DeadlyCorpsess <121748317+DeadlyCorpsess@users.noreply.github.com>
2025-12-23 13:15:27 -0500

    Update server.js

--- server.js
+++ server.js
@@ -7,35 +7,32 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve the main folder and the models folder
+// 1. Serve the main folder files (index.html, etc)
 app.use(express.static(__dirname));
+
+// 2. Serve the models folder explicitly
 app.use('/models', express.static(path.join(__dirname, 'models')));
 
 let players = {};
 
 io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+
     socket.on('join game', (username) => {
         players[socket.id] = {
             id: socket.id,
             username: username || "Guest",
-            x: 0, z: 0
+            x: (Math.random() - 0.5) * 4,
+            z: (Math.random() - 0.5) * 4
         };
+        // Send everyone to the new player
         socket.emit('init players', players);
+        // Tell everyone else about the new player
         socket.broadcast.emit('new player', players[socket.id]);
     });
 
-    socket.on('chat message', (data) => { io.emit('chat message', data); });
-
-    socket.on('move', (data) => {
-        if (players[socket.id]) {
-            players[socket.id].x = data.x;
-            players[socket.id].z = data.z;
-            io.emit('player moved', { id: socket.id, x: data.x, z: data.z });
-        }
-    });
-
-    socket.on('stop move', () => {
-        io.emit('player stopped', { id: socket.id });
+    socket.on('chat message', (data) => {
+        io.emit('chat message', data);
     });
 
     socket.on('disconnect', () => {
